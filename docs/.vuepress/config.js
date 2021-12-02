7d90cb3d30f6b2b715f4787fd0157841769c411b
qun <qun0297@hotmail.com>
2021-12-02 11:35:05 +0800

    add ci

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 // 网站必要的配置文件
 module.exports = {
     title: '三只小羊',
-    description: 'test blogs'
+    description: 'test blogs',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+      },
+  base:'/blog/',
+
   }
